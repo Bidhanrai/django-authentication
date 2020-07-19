19230afb3e110e8af9631c9ec925de54fa88b9a3
bidhanrai424@gmail.com <gitlab.com>
2020-07-19 17:40:51 +0545

    first commit

---
+++ userAuth/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+
+
