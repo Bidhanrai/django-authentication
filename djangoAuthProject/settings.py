6489f4a985e0883fbb8eca7e05701f0fe3594c21
bidhanrai424@gmail.com <gitlab.com>
2020-07-20 17:02:17 +0545

    added blog app and Post model for blog posts

--- djangoAuthProject/settings.py
+++ djangoAuthProject/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'userAuth'
+    'userAuth.apps.AuthConfig',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
