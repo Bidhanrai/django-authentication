721b0351e3d431b8c911f725738644c35e22585f
bidhanrai424@gmail.com <gitlab.com>
2020-08-10 16:00:13 +0545

    password reset email

--- djangoAuthProject/settings.py
+++ djangoAuthProject/settings.py
@@ -143,5 +143,12 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+# smtp email backend
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'demosampangrai@gmail.com'
+EMAIL_HOST_PASSWORD = 'hipsycgdebyhziot'
+DEFAULT_FROM_EMAIL = 'CodingWithSampang Team <noreply@codingwithsampang.com>'
+# EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
